bb41bbb24c7c77b24068c0f6586169a15232e4a5
jeisson333 <64706466+jeisson333@users.noreply.github.com>
2021-04-26 22:02:59 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -36,99 +36,61 @@ function principal(){
     var pobl = document.querySelector('.Pobl');
     var regio = document.querySelector('.Regio');
     var imgp = document.querySelector('.ImgP');
-
+    
    
     switch(pais){
-        case "Col":ciudadclim = "bogota";
+        case "Col":
         paisselec = "colombia";
-        iniciarMap(latCol,lngCol);
-        clima(ciudadclim);
+        
         paises(paisselec);
 
         break;
-        case "Bra": iniciarMap(latBra,lngBra);
-        ciudadclim = "Brasilia";
+        case "Bra": 
         paisselec = "brasil";
-        clima(ciudadclim);
+        
         paises(paisselec);
 
         break;
-        case "Arg": iniciarMap(latArg,lngArg);
-        ciudadclim = "Buenos Aires";
-        clima(ciudadclim);
+        case "Arg": 
         paisselec = "argentina";
         paises(paisselec);
 
         break;
-        case "Per": iniciarMap(latPer,lngPer);
-        ciudadclim = "Lima";
-        clima(ciudadclim);
+        case "Per": 
         paisselec = "peru";
         paises(paisselec);
         
         break;
-        case "Chi": iniciarMap(latChi,lngChi);
-        ciudadclim = "Santiago de Chile";
-        clima(ciudadclim);
+        case "Chi": 
         paisselec = "chile";
         paises(paisselec);
         
         break;
-        case "Ecu": iniciarMap(latEcu,lngEcu);
-        ciudadclim = "Quito";
-        clima(ciudadclim);
+        case "Ecu": 
         paisselec = "Ecuador";
         paises(paisselec);
 
         break
-        case "Ven": iniciarMap(latVen,lngVen);
-        ciudadclim = "Caracas";
-        clima(ciudadclim);
+        case "Ven": 
         paisselec = "venezuela";
         paises(paisselec);
 
         break
-        case "Bol": iniciarMap(latBol,lngBol);
-        ciudadclim = "Sucre";
-        clima(ciudadclim);
+        case "Bol": 
         paisselec = "bolivia";
         paises(paisselec);
 
         break
-        case "Urg": iniciarMap(latUrg,lngUrg);
-        ciudadclim = "Montevideo";
-        clima(ciudadclim);
+        case "Urg": 
+        
         paisselec = "uruguay";
         paises(paisselec);
 
         break
-        case "Par": iniciarMap(latPar,lngPar);
-        ciudadclim = "Asunción";
-        clima(ciudadclim);
+        case "Par": 
         paisselec = "paraguay";
         paises(paisselec);
         break
-    }
-    function clima(ciudadclim) {
-        fetch('https://api.openweathermap.org/data/2.5/weather?q='+ciudadclim+'&appid=cecd891ddec9801054aa8017c995ebb5')
-        .then(Response => Response.json())
-        .then(data => {
-            var nameValue = data['name'];
-            var tempValue = data['main']['temp'];            
-            var descValue = data['weather'][0]['description'];
-            var temminValue = data['main']['temp_min'];
-            var temmaxValue = data['main']['temp_max'];
-
-            
-            nombre.innerHTML ="Nombre: "+ nameValue;
-            temact.innerHTML ="Temperatura Actual: "+tempValue+`<br/>`;
-            temmin.innerHTML ="Temperatura Minima: "+temminValue+`<br/>`;
-            temmax.innerHTML ="Temperatura Maxima: "+temmaxValue+`<br/>`;
-            estado.innerHTML = "Estado: "+descValue;
-
-        })         
-            
-               
     }
     function paises(paisselec) {
 
@@ -141,31 +103,41 @@ function principal(){
             var poblValue = data['0']['population'];
             var regioValue = data['0']['region'];
             var imgpValue = data['0']['flag'];
-
+            
             nombp.innerHTML ="Pais: "+nompValue;
             cap.innerHTML ="Capital: "+capValue+`<br/>`;
             codp.innerHTML ="Codigo Pais: "+codpValue+`<br/>`;
             pobl.innerHTML ="Poblacion: "+poblValue+"<br/>";
             regio.innerHTML ="Region: "+regioValue+`<br/>`;
             imgp.innerHTML = '<img src="'+imgpValue+'" width="400" height="300">';
-
             
-        }) 
+            clima(capValue); 
+        })
+       
     }
-
-      
-
-
-
-}
-function iniciarMap(latt,lngg){
-    var coord = {lat:latt ,lng: lngg};
-    var map = new google.maps.Map(document.getElementById('map'),{
-      zoom: 5,
-      center: coord
-    });
-    /*var marker = new google.maps.Marker({
-      position: coord,
-      map: map
-    });*/
-}
+    function clima(ciudadclim) {
+        fetch('https://api.openweathermap.org/data/2.5/weather?q='+ciudadclim+'&appid=cecd891ddec9801054aa8017c995ebb5')
+        .then(Response => Response.json())
+        .then(data => {
+            var nameValue = data['name'];
+            var tempValue = data['main']['temp'];            
+            var descValue = data['weather'][0]['description'];
+            var temminValue = data['main']['temp_min'];
+            var temmaxValue = data['main']['temp_max'];
+            var latValue = data['coord']['lat'];
+            var lngValue = data['coord']['lon'];
+            
+            
+            nombre.innerHTML ="Nombre: "+ nameValue;
+            temact.innerHTML ="Temperatura Actual: "+tempValue+`<br/>`;
+            temmin.innerHTML ="Temperatura Minima: "+temminValue+`<br/>`;
+            temmax.innerHTML ="Temperatura Maxima: "+temmaxValue+`<br/>`;
+            estado.innerHTML = "Estado: "+descValue;
+            
+            iniciarMap(latValue,lngValue);   
+        })
+             
+            
+               
+    }
+    
